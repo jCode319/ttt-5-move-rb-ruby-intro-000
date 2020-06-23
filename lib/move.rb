373c9d64550c689142bf4cc3ab0538c54de347d2
Jarrell Jackson <jdjack3@gmail.com>
2020-06-23 01:39:09 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,6 +13,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, player)
-  board[index] = player
+def move(position, character, player = "X")
+  board[position] = character
 end
